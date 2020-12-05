10a5b34ae4b130836e93961b8f6c4f3afeaaeb85
wahudong <wahudong@gmail.com>
2020-12-05 14:03:36 -0600

    add model Project and before migrate2

--- time_track/models.py
+++ time_track/models.py
@@ -1,14 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Project(models.Model):
   projet_name = models.CharField(max_length=200)
   completed = models.BooleanField(default = False)
-  user = models.ForeignKey(user, on_delete=models.CASCADE)
+  operator = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
-        return "%s | %s" % (self.projet_name, self.user.username)
+        return "%s | %s" % (self.projet_name, self.operator.username)
 
 # class TimeList(models.Model):
 #   start_time =  models.DateTimeField(auto_now=False)
