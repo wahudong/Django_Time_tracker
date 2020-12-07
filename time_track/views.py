1fd8114de089fc2377a32f0c76a3a5696ec4dd7f
wahudong <wahudong@gmail.com>
2020-12-07 00:00:33 -0600

    decorating

--- time_track/views.py
+++ time_track/views.py
@@ -12,22 +12,35 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 from django.urls import reverse
+import json
+from django.http import HttpResponse
+from django.http import JsonResponse
+
 
 # Create your views here.
 
 def home(request):
   if request.user.is_authenticated:
-    # if request.method == 'POST':
-    #   form = ProjectForm(request.POST or None)
-    #   if form.is_valid():
-    #     form.save()
-    #     # user_projects = Project.objects.get(pk=1)
-    #     messages.success(request,('The project has been created.'))
-    #     return render(request, 'home.html', {'user': request.user})
-    # else:
-      # user_projects = Project.objects.get(pk=1)
-      projects_of_user =  Project.objects.filter(operator = request.user)
-      return render(request, 'home.html', {'user': request.user, 'projects_of_user':projects_of_user})
+    mytest = 'Not a POST'
+    hours = 0
+    if request.method == 'POST':
+      mytest = request.POST
+      hours_unicode = request.body.decode('utf-8')
+      # hours = json.load(hours_unicode)
+      # hour = hours[0]
+
+
+
+        # # form = ProjectForm(request.POST or None)
+        # if form.is_valid():
+        #   form.save()
+        #   # user_projects = Project.objects.get(pk=1)
+        #   messages.success(request,('The project has been created.'))
+        #   return render(request, 'home.html', {'user': request.user})
+
+    projects_of_user =  Project.objects.filter(operator = request.user)
+    return render(request, 'home.html', {'hours':hours,'mytest':mytest,'user': request.user, 'projects_of_user':projects_of_user})
+
   else:
     return redirect('login')
 
@@ -100,6 +113,7 @@ def create_project(request):
         form.save()
         projects_of_user =  Project.objects.filter(operator = request.user)
         messages.success(request,('The project has been created.'))
+        # request.method == ''
         return redirect('home')
     else:
       # user_projects = Project.objects.get(pk=1)
